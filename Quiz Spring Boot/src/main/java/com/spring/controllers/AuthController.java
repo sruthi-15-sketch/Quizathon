eed841854938dee5f4676ab900fa947631c0bcdd
Sruthi Boddu <saisiva1715@gmail.com>
2022-10-01 01:13:10 +0530

    Created Entity table

---
+++ Quiz Spring Boot/src/main/java/com/spring/controllers/AuthController.java
@@ -0,0 +1,74 @@
+package com.spring.controllers;
+
+import com.spring.models.User;
+import com.spring.repository.UserRepository;
+import com.spring.request.LoginRequest;
+import com.spring.request.SignupRequest;
+import com.spring.response.JwtResponse;
+import com.spring.response.MessageResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.security.jwt.JwtUtils;
+import com.spring.security.services.UserDetailsImpl;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+    UserRepository userRepository;
+
+	@Autowired
+	PasswordEncoder encoder;
+
+	@Autowired
+	JwtUtils jwtUtils;
+
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateuser(@RequestBody LoginRequest loginRequest) {
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtUtils.generateJwtToken(authentication);
+
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+		return ResponseEntity
+				.ok(new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail()));
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@RequestBody SignupRequest signUpRequest) {
+		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+			return ResponseEntity.badRequest().body(new MessageResponse("Error: username is already taken!"));
+		}
+
+		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+			return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+		}
+
+		// Create new user account
+		User user = new User(signUpRequest.getUsername(), signUpRequest.getEmail(),
+				encoder.encode(signUpRequest.getPassword()));
+
+		userRepository.save(user);
+
+		return ResponseEntity.ok(new MessageResponse("user registered successfully!"));
+	}
+}
---
+++ Quiz Spring Boot/src/main/java/com/spring/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.spring.controllers;
+
+import com.spring.response.MessageResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 4800)
+@RestController
+@RequestMapping("/api/test")
+public class UserController {
+
+	@GetMapping("/all")
+	public MessageResponse allAccess() {
+		return new MessageResponse("Server is up.....");
+	}
+
+	@GetMapping("/greeting")
+	@PreAuthorize("isAuthenticated()")
+	public MessageResponse userAccess() {
+
+		return new MessageResponse("Congratulations! You are an authenticated user.");
+	}
+
+}
